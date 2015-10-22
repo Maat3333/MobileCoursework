583f4412d7343c19582aaa53cc18d8d05d9bc972
Maat3333 <matthewmetcalfe1991@hotmail.co.uk>
2015-10-22 21:29:03 +0100

    Added new layout with the start of the maps activity, added some background ideas

--- app/src/main/java/com/example/matthew/mobilecoursework/MainActivity.java
+++ app/src/main/java/com/example/matthew/mobilecoursework/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.matthew.mobilecoursework;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testButton = (Button) findViewById(R.id.button1);
+        testButton.setOnClickListener(this);
+        Log.e("n", "got to here");
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent test = new Intent(getApplicationContext(),MapsActivity.class);
+
+        Log.e("n", "got to here");
+        startActivity(test);
     }
 }
