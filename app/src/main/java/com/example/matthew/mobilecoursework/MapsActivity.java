5c818c55ba7fe2209436460b13a3c76d01fdce08
Maat3333 <matthewmetcalfe1991@hotmail.co.uk>
2015-10-22 23:45:15 +0100

    Added more maps features( 2 points on map with line inbetween) trying to add using current loaction but need to refactor as using code for wrong api

--- app/src/main/java/com/example/matthew/mobilecoursework/MapsActivity.java
+++ app/src/main/java/com/example/matthew/mobilecoursework/MapsActivity.java
@@ -1,7 +1,18 @@
 package com.example.matthew.mobilecoursework;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +20,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +55,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location loc) {
+                //editLocation.setText("");
+
+                // pb.setVisibility(View.INVISIBLE);
+                Toast.makeText(
+                        getBaseContext(),
+                        "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                                + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+                double longitude = loc.getLongitude();
+                // Log.v(TAG, longitude);
+                Double latitude = loc.getLatitude();
+                //Log.v(TAG, latitude);
+                home = new LatLng(latitude, longitude);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//TODO: this is wanting to use api 23 my phone only supporst 22? this needs to be changed
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        mMap.addMarker(new MarkerOptions().position(home).title("Marker in home"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+
+
+        LatLng chome = new LatLng(54.543656, -1.922299);
+        mMap.addMarker(new MarkerOptions().position(chome).title("Marker in c"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom((chome), 100f));
+
+
+
+
+
+        mMap.addPolyline(new PolylineOptions().geodesic(true)
+                .add(home)  // Sydney
+                .add(chome)  // Fiji
+        );
+
     }
 }
